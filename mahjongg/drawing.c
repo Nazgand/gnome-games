fe67e7f530bb5838045f0e03bdac344543b62897
Thomas Hindoe Paaboel Andersen <phomes@gmail.com>
2009-09-01 01:17:10 +0200

    Gseal: mostly allocation fixes
    Bug #586731

--- mahjongg/drawing.c
+++ mahjongg/drawing.c
@@ -20,6 +20,7 @@
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
 
+#include <libgames-support/games-gtk-compat.h>
 #include <libgames-support/games-preimage.h>
 #include <libgames-support/games-runtime.h>
 
